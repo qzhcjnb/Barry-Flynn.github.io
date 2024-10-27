9c671a8b8c8749ada63e0f9b0a3afe7fa5765991
百里飞洋 <Barry-Flynn@qq.com>
2024-10-27 23:42:12 +0800

    Site updated: 2024-10-27 23:40:26

--- js/console.js
+++ js/console.js
@@ -3,3 +3,12 @@
 
 console.log("\n%c 百里飞洋的小世界 %c https://blog.meta-code.top \n","color: #ffffff; background: #005aff; padding:5px 0;","background: #b9ddff; padding:5px 0;");
 console.log("\n%c 【郑重声明】 %c 禁止运用计算机技术对本网站进行非法攻击甚至恶意破坏。本网站已使用一些安全技术对网站内容进行保护，并对攻击者的各项行为、IP地址等进行记录并提交给相关部门。对于有邪恶念头者，本网站零容忍，将会全力配合网警工作，提供非法技术操作者的信息。中国是网络安全的坚定维护者，中方历来坚决反对并依法打击任何形式的网络攻击和窃密行为。《中华人民共和国网络安全法》第27条规定，任何个人和组织不得从事非法侵入他人网络、干扰他人网络正常功能、窃取网络数据等危害网络安全的活动。该类活动构成犯罪并将受到法律的制裁。 \n","color: #ffffff; background: #005aff; padding:5px 0;","background: #b9ddff; padding:5px 0;");
+
+var hostname1 = 'blog' + '.' + 'meta' + '-' + 'code' + '.top'
+var hostname2 = 'barry' + '-' + 'flynn' + '.' + 'github' + '.' + 'io'
+var hostname3 = 'localhost'
+var hostnameNow = document.location.hostname
+if (hostnameNow !== hostname1 && hostnameNow !== hostname2 && hostnameNow !== hostname3) {
+	var sourceDomain = 'https://' + hostname1
+	window.location.href = sourceDomain + document.location.pathname
+}
